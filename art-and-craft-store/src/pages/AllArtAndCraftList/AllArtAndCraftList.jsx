d145c9f98f0a060e3428dacf0555857e8ea45b92
Developer Barry <barryallen2558@gmail.com>
2024-12-19 23:23:16 +0600

    apply conditional css

--- art-and-craft-store/src/pages/AllArtAndCraftList/AllArtAndCraftList.jsx
+++ art-and-craft-store/src/pages/AllArtAndCraftList/AllArtAndCraftList.jsx
@@ -61,7 +61,9 @@ const AllArtAndCraftList = () => {
 
 
     const handleNext = () => {
-
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1)
+        }
     }
 
 
@@ -141,8 +143,9 @@ const AllArtAndCraftList = () => {
                             ))}
 
                             <li
-                                // onClick={handleNext}
-                                className="flex items-center justify-center shrink-0 border hover:border-blue-500 cursor-pointer w-9 h-9 rounded-md">
+                                onClick={handleNext}
+                                aria-disabled={currentPage === pages.length - 1}
+                                className={`${currentPage === pages.length - 1 ? 'cursor-not-allowed bg-gray-100 hover:border-none' : ''} flex items-center justify-center shrink-0 border hover:border-blue-500 cursor-pointer w-9 h-9 rounded-md`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-3 fill-gray-400 rotate-180" viewBox="0 0 55.753 55.753">
                                     <path
                                         d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
